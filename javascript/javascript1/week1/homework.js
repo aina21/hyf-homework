c2a90c06b33d9156361017f4ce1bd75b9a950cfe
aina21 <ida.naderian@gmail.com>
2019-05-21 00:43:30 +0200

    add first javascript homework

---
+++ javascript/javascript1/week1/homework.js
@@ -0,0 +1,70 @@
+/**
+ * Javascript file for Hack Your Future, Javascript, homework Week1
+ * author: Ida Naderian
+ * this file includes
+  1. Age-ify (A future age calculator)
+  2. Goodboy-Oldboy (A dog age calculator)
+  3. Housey pricey (A house price estimator)
+  4. Ez Namey (Startup name generator) 
+ */
+
+//  1. Age-ify (A future age calculator)
+let yearOfBirth = 1987;
+let yearFuture = 2045;
+
+let age = yearFuture - yearOfBirth;
+
+console.log("Your age will be "+ age);
+
+//  2. Goodboy-Oldboy (A dog age calculator)
+let dogYearOfBirth = 2017;
+let dogYearFuture = 2027;
+
+let dogYear = dogYearFuture - dogYearOfBirth;
+
+let shouldShowResultInDogYears = true;
+
+if(shouldShowResultInDogYears){
+    console.log("Your dog will be " + dogYear + " human years old in " + yearFuture);
+}
+else{
+    console.log("Your dog will be " + (dogYear * 7) +" dog years old in " + yearFuture);
+}
+
+//  3. Housey pricey (A house price estimator)
+//input data
+let myFriend = ["Peter", "Juli"];
+let houseWide = [8, 5];
+let houseDeep = [10, 11];
+let houseHigh = [10, 8];
+let gardenSizeInM2 = [100, 70];
+let paidPrice = [2500000, 1000000];
+
+let volumeInMeters = 0;
+let housePrice = 0;
+
+for(let i = 0; i < 2; i++){
+    volumeInMeters = houseHigh[i] * houseDeep[i] * houseWide[i];
+    housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2[i] * 300;
+
+    if(housePrice >= paidPrice[i])
+    {
+        console.log(myFriend[i] + ", You got it from a good price")
+    }
+    else
+    {
+        console.log(myFriend[i] + ", It's too expensive");
+    }
+}
+
+// 4. Ez Namey (Startup name generator) 
+let firstWords = ["Easy", "Awesome", "Corporate", "Amazing", "Magnificent", "Wonderful", 
+"Phenomenal", "Impressive", "Incredible", "Unbelievable"];
+let secondWords = ["Company", "Operation", "Corporation", "Partnership", "Agency", "Institution", 
+"multinational", "Group", "Business", "Firm"];
+
+const randomNumber = [(Math.floor(Math.random() * 10) + 0) , (Math.floor(Math.random() * 10) + 0)];
+let newStartupName = firstWords[randomNumber[0]] + " " + secondWords[randomNumber[1]]
+
+console.log("The startup: \"" + newStartupName + "\" contains " + newStartupName.length + " characters");
+
