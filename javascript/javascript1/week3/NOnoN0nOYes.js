40c363f46bf3ffbea23bafc37a0bcc2829684a2a
aina21 <ida.naderian@gmail.com>
2019-06-07 11:43:14 +0200

    improve NOnoNOnYes

--- javascript/javascript1/week3/NOnoN0nOYes.js
+++ javascript/javascript1/week3/NOnoN0nOYes.js
@@ -6,10 +6,12 @@
  */
 
  const notes = [];
+ //errors list
  const ERROR = {
     NOTFIND : "Note is not in list"
  }
 
+ //add note to database
  function addNote(id, content){
     notes.push({
         content: content,
@@ -17,6 +19,7 @@
     });
  }
 
+ //get note from id
  function getNoteFromId(id){
      const note = notes.filter(item => item.id === id);
      if (!Array.isArray(note) || !note.length){
@@ -25,16 +28,19 @@
      return note;
  }
 
- function getAllNotes(){
+ //get all notes list
+ function getNotes(){
     return notes;
  }
 
+ //logout note formatted
  function logOutNotesFormatted(){
      for(note of notes){
         console.log(`The note with ${note.id}, has the following note text: ${note.note}`);
      }
  }
 
+ //run
  addNote(1, "Hello It's a first note");
  addNote(2, "Goodmorning It's a second note");
  addNote(3, "Have a good day, It's my last note");
