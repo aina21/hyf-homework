1906e32b667c58b2492ae64c15288999918f6c2c
aina21 <ida.naderian@gmail.com>
2019-06-08 18:45:50 +0200

    fixed an issue in NOnoNOnOYes program about create same id

--- javascript/javascript1/week3/NOnoN0nOYes.js
+++ javascript/javascript1/week3/NOnoN0nOYes.js
@@ -5,45 +5,59 @@
  * program will create note and save it
  */
 
- const notes = [];
- //errors list
- const ERROR = {
-    NOTFIND : "Note is not in list"
- }
+const notes = [];
+//errors list
+const ERROR = {
+    NOTFIND: "Note is not in list"
+}
 
- //add note to database
- function addNote(id, content){
-    notes.push({
-        content: content,
-        id: id
-    });
- }
+//add note to database
+function addNote(id, content) {
+    if (!isNoteInList(id)) {
+        notes.push({
+            content: content,
+            id: id
+        });
+    } else {
+        throw console.error("Note is alreadt exist.");
+    }
+}
 
- //get note from id
- function getNoteFromId(id){
-     const note = notes.filter(item => item.id === id);
-     if (!Array.isArray(note) || !note.length){
-         return ERROR.NOTFIND;
-     }
-     return note;
- }
+function isNoteInList(id) {
+    for (note of notes) {
+        if (note.id === id) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+//get note from id
+function getNoteFromId(id) {
+    const note = notes.filter(item => item.id === id);
+    if (!Array.isArray(note) || !note.length) {
+        return ERROR.NOTFIND;
+    }
+    return note;
+}
 
- //get all notes list
- function getNotes(){
+//get all notes list
+function getNotes() {
     return notes;
- }
+}
 
- //logout note formatted
- function logOutNotesFormatted(){
-     for(note of notes){
-        console.log(`The note with ${note.id}, has the following note text: ${note.note}`);
-     }
- }
+//logout note formatted
+function logOutNotesFormatted() {
+    for (note of notes) {
+        console.log("The note with " + note.id + " has the following note text: " + note.note);
+    }
+}
 
- //run
- addNote(1, "Hello It's a first note");
- addNote(2, "Goodmorning It's a second note");
- addNote(3, "Have a good day, It's my last note");
- addNote(1, "Hello It's a first note");
+//run
+addNote(1, "Hello It's a first note");
+addNote(2, "Goodmorning It's a second note");
+addNote(3, "Have a good day, It's my last note");
+addNote(1, "Hello It's a first note");
 
- console.log(getNoteFromId(0));
+console.log(getNoteFromId(1));
+console.log(isIdUniq(1));
