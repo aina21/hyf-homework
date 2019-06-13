80d5e45961bacf0701a519982f8b67b238693dfc
aina21 <ida.naderian@gmail.com>
2019-06-13 16:02:49 +0200

    delete test file

--- javascript/javascript2/week1/spiritAnimalNameGenerator/test.js
+++
@@ -1,9 +0,0 @@
-const danishWords = ['bil', 'plante', 'kaffe', 'bog', 'ø', 'planetarium'];
-function findShortest(){
-const shortest = '';
-for(words of danishWords){
-  if(words.lenghth < shortest.length) shortest = words;
-}
-return shortest;
-}
-console.log(findShortest(danishWords));
