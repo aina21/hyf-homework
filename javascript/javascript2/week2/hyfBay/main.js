0cc58e5203b8420dcdcb14e0547a7b99abe0a592
aina21 <ida.naderian@gmail.com>
2019-06-21 10:33:14 +0200

    solved issue for search a country

--- javascript/javascript2/week2/hyfBay/main.js
+++ javascript/javascript2/week2/hyfBay/main.js
@@ -18,7 +18,7 @@ const searchInput = document.querySelector('.search > input');
 const countrySelect = document.querySelector('.country > select');
 const sortSelect = document.querySelector('.sort > select');
 
-const SORTOPTION={
+const SORTOPTION = {
     NAME: 'name',
     EXPENSIVE: 'expensive',
     CHEAP: 'cheap'
@@ -31,11 +31,22 @@ const SORTOPTION={
  */
 function renderProducts(list) {
     const ul = document.querySelector("section.products > ul");
+
+
     clearList(ul);
     for (const product of list) {
+        // const btnCart = document.createElement('button');
+        // btnCart.innerHTML = "Add to cart"
         const li = document.createElement('li');
+
+        // li.addEventListener('click',function(){
+        //     const 
+        // })
+
         li.appendChild(createList(product));
+        // li.appendChild(btnCart);
         ul.appendChild(li);
+
     }
 
 }
@@ -49,16 +60,16 @@ function renderProducts(list) {
 function createList(parent) {
     const ul = document.createElement('ul');
     const keys = Object.keys(parent);
+
     for (const key of keys) {
         const li = document.createElement('li');
-        if(Array.isArray(parent[key])){
+        if (Array.isArray(parent[key])) {
             li.appendChild(createList(parent[key]));
         } else {
             li.innerHTML = parent[key];
             li.setAttribute('class', key)
         }
         ul.appendChild(li);
-
     }
     return ul;
 }
@@ -68,7 +79,7 @@ function createList(parent) {
  *
  * @param {*} parent => parent node 
  */
-function clearList(parent){
+function clearList(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
@@ -77,38 +88,60 @@ function clearList(parent){
 renderProducts(products);
 
 //search name of product
-searchInput.addEventListener('change', function(){
+searchInput.addEventListener('change', function () {
     result = products.filter((item) => {
         return item.name.includes(searchInput.value);
     })
     renderProducts(result);
 })
 
+
+
 //Showing products that ships to country
-countrySelect.addEventListener('change', function(){
-    result = products.filter((item) => {
-        return item.shipsTo.includes(countrySelect.value);
-    })
+// countrySelect.addEventListener('change', function(){
+//     result = products.filter((item) => {
+//         return item.shipsTo.includes(countrySelect.value);
+//     })
+//     renderProducts(result);
+//     // console.log(countrySelect.text);
+// })
+
+function convertToLowerCase(str) {
+    return str.trim().toLocaleLowerCase();
+}
+
+countrySelect.addEventListener('change', function () {
+    const selectedCountryName = convertToLowerCase(countrySelect.value);
+
+    result = products.filter(item => {
+        return item.shipsTo.some(shippingCountry => {
+            return convertToLowerCase(shippingCountry) === selectedCountryName;
+        });
+    });
+
     renderProducts(result);
-    console.log(countrySelect.text);
-})
+
+});
 
 //sort list
-sortSelect.addEventListener('change', function(){
+sortSelect.addEventListener('change', function () {
     let result = products;
-    if(sortSelect.value === SORTOPTION.NAME){
-        result = products.sort((obj1, obj2)=> {
+    if (sortSelect.value === SORTOPTION.NAME) {
+        result = products.sort((obj1, obj2) => {
             return obj1.name > obj2.name ? 1 : -1;
         });
-    } else if (sortSelect.value === SORTOPTION.CHEAP){
-        result = products.sort((obj1,obj2) =>{
+    } else if (sortSelect.value === SORTOPTION.CHEAP) {
+        result = products.sort((obj1, obj2) => {
             return obj1.price > obj2.price ? 1 : -1;
         })
-    } else if (sortSelect.value === SORTOPTION.EXPENSIVE){
-        result = products.sort((obj1,obj2) =>{
+    } else if (sortSelect.value === SORTOPTION.EXPENSIVE) {
+        result = products.sort((obj1, obj2) => {
             return obj1.price < obj2.price ? 1 : -1;
-        })    }
+        })
+    }
 
     renderProducts(result)
 
 })
+
+//add to the cart
