fb1325e98905331e05c8d95aa74b4a07a54b2c90
aina21 <ida.naderian@gmail.com>
2019-06-21 14:05:13 +0200

    counted the total number of Good, Average and Bad movies

--- javascript/javascript2/week2/workingWithMovies/scripts/main.js
+++ javascript/javascript2/week2/workingWithMovies/scripts/main.js
@@ -10,54 +10,56 @@ const movies = getMovies();
 
 //movies with a short title
 const shortTitleMovies = movies.filter(movie => {
-    if (movie.title.length < 5) {
-        return movie;
-    }
+  if (movie.title.length < 5) {
+    return movie;
+  }
 });
 
 //long movie titles
 const longTitleMovies = movies.filter(movie => {
-    if (movie.title.length >= 50) {
-        return movie;
-    }
+  if (movie.title.length >= 50) {
+    return movie;
+  }
 });
 
 //number of movies made between 1980-1989 (including both the years)
 const numberOfMoviesBetween1980To1989 = movies.reduce((total, movie) => {
-    if (movie.year > 1980 && movie.year < 1989) {
-        total++;
-    }
-    return total;
+  if (movie.year > 1980 && movie.year < 1989) {
+    total++;
+  }
+  return total;
 }, 0);
 
 //tag array
 const tag = {
-    GOOD: "Good",
-    AVERAGE: "Average",
-    BAD: "Bad"
+  GOOD: "Good",
+  AVERAGE: "Average",
+  BAD: "Bad"
 };
 
 /**
  * create tag for movies array
  */
 function createTag() {
-    movies.forEach(movie => {
-        if (movie.rating >= 7) {
-            movie["tag"] = tag.GOOD;
-        } else if (movie.rating < 7 && movie.rating >= 4) {
-            movie["tag"] = tag.AVERAGE;
-        } else {
-            movie["tag"] = tag.BAD;
-        }
-    });
+  movies.forEach(movie => {
+    if (movie.rating >= 7) {
+      movie["tag"] = tag.GOOD;
+    } else if (movie.rating < 7 && movie.rating >= 4) {
+      movie["tag"] = tag.AVERAGE;
+    } else {
+      movie["tag"] = tag.BAD;
+    }
+  });
 }
+
+createTag();
 
 //array to only the rating of the movies rated higher than 66
 const rateMoreThan6 = movies
-    .filter(movie => {
-        if (movie.rating > 6) return movie;
-    })
-    .map(movie => movie.rating);
+  .filter(movie => {
+    if (movie.rating > 6) return movie;
+  })
+  .map(movie => movie.rating);
 
 /**
  * looking for a word in title of movies
@@ -66,78 +68,100 @@ const rateMoreThan6 = movies
  * @returns => list of movies includes
  */
 function searchMoviesWithTitle(word) {
-    const result = movies.filter(movie => {
-        if (movie.title.toLowerCase().includes(word.toLowerCase())) return movie;
-    });
-    return result;
+  const result = movies.filter(movie => {
+    if (movie.title.toLowerCase().includes(word.toLowerCase())) return movie;
+  });
+  return result;
 }
 
 //Count the total number of movies containing any of following keywords: Surfer, Alien or Benjamin
 const totalNumberOfMovies =
-    searchMoviesWithTitle("Surfer ").length +
-    searchMoviesWithTitle("AlIeN ").length +
-    searchMoviesWithTitle("Benjamin").length;
+  searchMoviesWithTitle("Surfer ").length +
+  searchMoviesWithTitle("AlIeN ").length +
+  searchMoviesWithTitle("Benjamin").length;
 // console.log(searchMoviesWithTitle('Surfer'));
 
 //duplicate
 //find words in string that duplicated and return into array
-String.prototype.wordDuplicate = function () {
-    const list = this.split(' ');
-    return list.filter((item, index) => list.indexOf(item) !== index);
+String.prototype.wordDuplicate = function() {
+  const list = this.split(" ");
+  return list.filter((item, index) => list.indexOf(item) !== index);
 };
 
 //array of movies where a word in the title is duplicated
 const arrayOfDuplicateTitle = movies.filter(item => {
-    if (item.title.wordDuplicate().length !== 0) {
-        return item;
-    }
+  if (item.title.wordDuplicate().length !== 0) {
+    return item;
+  }
 });
 
 //mostly duplicated word in title
 function maxDuplicatedWord(title) {
-    title = title.wordDuplicate().sort();
+  title = title.wordDuplicate().sort();
 
-    let cnt = 0;
-    let max = 0;
-    let current = title[0];
+  let cnt = 0;
+  let max = 0;
+  let current = title[0];
 
-    for (const word of title) {
-        if (word !== current) {
-            if (cnt > max) {
-                max = cnt;
-            }
-            cnt = 1;
-            current = word;
-        } else {
-            cnt++;
-        }
-    }
-
-    if (cnt > max) {
-        return {
-            word: current,
-            value: cnt + 1
-        };
+  for (const word of title) {
+    if (word !== current) {
+      if (cnt > max) {
+        max = cnt;
+      }
+      cnt = 1;
+      current = word;
     } else {
-        return {
-            word: current,
-            value: max + 1
-        };
+      cnt++;
     }
+  }
+
+  if (cnt > max) {
+    return {
+      word: current,
+      value: cnt + 1
+    };
+  } else {
+    return {
+      word: current,
+      value: max + 1
+    };
+  }
 }
 
-//  find the max duplicated word
-maxDuplicatedTitle = movies.reduce(
-    (mostDuplicate, item) => {
-        mostDuplicate =
-            maxDuplicatedWord(item.title).value > mostDuplicate.value ?
-            maxDuplicatedWord(item.title) :
-            mostDuplicate;
-        return mostDuplicate;
-    }, {
-        word: "",
-        value: 0
-    }
+// find the max duplicated word
+const maxDuplicatedTitle = movies.reduce(
+  (mostDuplicate, item) => {
+    mostDuplicate =
+      maxDuplicatedWord(item.title).value > mostDuplicate.value
+        ? maxDuplicatedWord(item.title)
+        : mostDuplicate;
+    return mostDuplicate;
+  },
+  {
+    word: "",
+    value: 0
+  }
 );
 
-console.log(maxDuplicatedTitle);
+// Calculate the average rating of all the movies
+const averageOfAllMovies = movies.reduce((sum, item) => {
+    sum += item.rating;
+    return sum;
+},0) / movies.length;
+
+//Count the total number of Good, Average and Bad movies
+const total= movies.reduce((total, item) => {
+    if(item.tag === tag.GOOD){
+        total.totalGood = total.totalGood + 1;
+    } else if(item.tag === tag.AVERAGE){
+        total.totalAverage = total.totalAverage + 1;
+    } else {
+        total.totalBad = total.totalBad + 1;
+    }
+    return total;
+},{
+    totalGood: 0,
+    totalAverage: 0,
+    totalBad: 0
+})
+console.log(total);
