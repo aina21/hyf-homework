2e6c3055b33ddee622265e37bef68c723329c7ba
Ida <ida.naderian@gmail.com>
2019-08-27 09:29:00 +0200

    Meal queries

---
+++ databases/week3/mealSharing/meal.sql
@@ -0,0 +1,30 @@
+USE meal_sharing_website;
+-- Get all meals
+-- SELECT * FROM Meal
+
+-- Add a new meal
+-- INSERT INTO Meal
+-- VALUES
+-- (NULL,
+-- 'Butter Chciken',
+-- 'Boneless chicken cooked in oven and finished in butter with aromatic Indian spices, cream and cashews',
+-- 'Helsingborg, Skåne lä',
+-- '2019-09-02',
+-- 10,
+-- 12.00,
+-- now());
+
+-- Get a meal with any id, fx 1
+-- SELECT * FROM Meal 
+-- WHERE meal.id = 1;
+
+-- Update a meal with any id, fx 1. Update any attribute fx the title or multiple attributes
+-- UPDATE Meal 
+-- SET max_reservation = 4, price = 10.00
+-- WHERE Meal.id = 1
+
+-- Delete a meal with any id, fx 1
+-- DELETE FROM Meal 
+-- WHERE Meal.id = 1;
+
+
