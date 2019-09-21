1191d9119510c256d8f6cd688c6354fef175e2c6
Ida <ida.naderian@gmail.com>
2019-09-21 07:41:21 +0200

    Fix issues for week2 nodejs.

--- nodejs/week2/mealSharingWebsite/src/backend/index.js
+++ nodejs/week2/mealSharingWebsite/src/backend/index.js
@@ -11,12 +11,9 @@ const meals = require("./routes/meals");
 const reservations = require("./routes/reservations");
 const reviews = require("./routes/reviews");
 
-app.get("/meals/:id", meals);
-app.get("/meals", meals);
-app.get("/reservations/:id", reservations);
-app.get("/reservations", reservations);
-app.get("/reviews/:id", reviews);
-app.get("/reviews", reviews);
+app.use("/meals", meals);
+app.use("/reservations", reservations);
+app.use("/reviews", reviews);
 
 const server = app.listen(3000, () => {
   console.log("The app is running on localhost:3000!");
--- nodejs/week2/mealSharingWebsite/src/backend/routes/reservations.js
+++ nodejs/week2/mealSharingWebsite/src/backend/routes/reservations.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const reservations = require("../data/reservations.json");
 
-router.get("/reservations/:id", function(req, res) {
+router.get("/:id", function(req, res) {
   const id = Number(req.params.id);
   const result = reservations.filter(reservation => {
     return reservation.mealId === id;
@@ -11,7 +11,7 @@ router.get("/reservations/:id", function(req, res) {
   res.send(result);
 });
 
-router.get("/reservations", function(req, res) {
+router.get("/", function(req, res) {
   res.send(reservations);
 });
 
